509d170f89368148fbe6f2d497eb2cd38dbd6d1b
Piotr Luszczek <luszczek@icl.utk.edu>
2023-05-14 12:36:01 -0400

    Add a stub for the main argument parser

--- sabath/__main__.py
+++ sabath/__main__.py
@@ -8,7 +8,20 @@ if sys.version_info < (3,):
     raise RuntimeError("Only Python 3 supported")
 
 
+import argparse
+
+
+def cmdparse(argv):
+    parser = argparse.ArgumentParser(prog=argv[0],
+        description="SABATH: Surrogate AI Benchmarking Applications' Testing Harness",
+        epilog="The project is currently under development and new commands are possible in the future.",
+        prefix_chars="-",
+        usage="%(prog)s <command> [sub-command] [options]")
+    return  parser.parse_args(args=argv[1:])
+
+
 def main(argv):
+    cmdargs = cmdparse(argv)
     return 0
 
 
