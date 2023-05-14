20eb34e20621e697c5aa73be34b1aa106d08f85a
Piotr Luszczek <luszczek@icl.utk.edu>
2023-05-14 12:30:04 -0400

    Add dummy main() as the main entry point

--- sabath/__main__.py
+++ sabath/__main__.py
@@ -6,3 +6,11 @@ import sys
 
 if sys.version_info < (3,):
     raise RuntimeError("Only Python 3 supported")
+
+
+def main(argv):
+    return 0
+
+
+if "__main__" == __name__:
+    sys.exit(main(sys.argv))
