8e0bde87b39c082d7332ee4e277014e264433d51
Piotr Luszczek <luszczek@icl.utk.edu>
2023-07-06 02:35:04 +0000

    Finish simple fetch command

--- sabath/__main__.py
+++ sabath/__main__.py
@@ -12,7 +12,7 @@ Testing Harness).
 
 
 import sys
-from .commands import fetch
+from .commands import dispatch, fetch
 
 
 if sys.version_info < (3,):
@@ -20,11 +20,6 @@ if sys.version_info < (3,):
 
 
 import argparse, os
-
-
-root = os.getenv("SABATH_ROOT")
-if root is None:
-    root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 
 def cmdparse(argv):
@@ -53,27 +48,13 @@ def cmdparse(argv):
     ):
         parsers[cmd] = actparser.add_parser(cmd, help=hlp)
 
-    parsers["fetch"].add_argument("model", nargs=1, help="Name of the model to fetch.")
-    parsers["fetch"].add_argument("dataset", nargs=1, help="Name of the dataset to fetch.")
+    parsers["fetch"].add_argument("--model", type=str, help="Name of the model to fetch.")
+    parsers["fetch"].add_argument("--dataset", type=str, help="Name of the dataset to fetch.")
 
     parsers["run"].add_argument("model", help="Name of the model to run.")
     parsers["run"].add_argument("dataset", help="Name of the dataset to run.")
 
     return mainparser.parse_args(args=argv[1:])
-
-
-def cmddispatch(args):
-    global root
-
-    if args.root:
-        root = args.root
-
-    if not os.path.exists(root):
-        print("Root path {} doesn't exist".format(root), file=sys.stderr)
-        raise FileNotFoundError
-
-    if "fetch" == args.command:
-        commands.fetch(args)
 
 
 def main(argv):
@@ -83,7 +64,7 @@ def main(argv):
             "Please specify one of the available commands or use '-h' flag for more informatin.")
         return 127
 
-    cmddispatch(cmdargs)
+    dispatch(cmdargs)
     return 0
 
 
--- sabath/commands.py
+++ sabath/commands.py
@@ -10,5 +10,32 @@ SABATH commands
 """
 
 
+import json, os, subprocess
+import sabath
+
+
+def git(cmd, *args):
+    return subprocess.Popen(("git", cmd) + args, executable="git").wait()
+
 def fetch(args):
-    pass
+    if args.model:
+        model = json.load(open(os.path.join(sabath.root, "var", "sabath", "repos", "builtin", "models", args.model[0],  args.model + ".json")))
+        if "git" in model:
+            cchpth = os.path.join(sabath.cache, args.model[0], args.model)
+            if not os.path.exists(cchpth):
+                os.makedirs(cchpth, exist_ok=True)
+
+            if git("clone", model["git"], cchpth):
+                raise RuntimeError
+
+
+def dispatch(args):
+    if args.root:
+        sabath.root = args.root
+
+    if not os.path.exists(sabath.root):
+        print("Root path {} doesn't exist".format(sabath.root), file=sys.stderr)
+        raise FileNotFoundError
+
+    if "fetch" == args.command:
+        fetch(args)
