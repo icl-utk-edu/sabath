c51d7b517ba5ff0d5e8fd37500e4a8218a8bf678
Piotr Luszczek <luszczek@icl.utk.edu>
2023-07-24 15:20:00 +0000

    Fix incorrect ordering of cache definition and use

--- sabath/__init__.py
+++ sabath/__init__.py
@@ -6,12 +6,12 @@
 import os, logging
 
 
-logging.basicConfig(filename=os.path.join(cache, "sabath.log"), encoding="utf-8", level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(message)s")
-
 root = os.getenv("SABATH_ROOT")
 if root is None:
     root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 cache = os.path.join(root, "var", "sabath", "cache")
+
+logging.basicConfig(filename=os.path.join(cache, "sabath.log"), encoding="utf-8", level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(message)s")
 
 __all__ = ["cache", "root"]
