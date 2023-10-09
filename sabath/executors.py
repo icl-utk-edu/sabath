ca50fdb578e7190cdc02112dbd8da760de6269d4
Piotr Luszczek <luszczek@icl.utk.edu>
2023-10-09 04:36:16 +0000

    Add class to deal with single level of dotted keys
    Fixes #18

--- sabath/executors.py
+++ sabath/executors.py
@@ -1,14 +1,12 @@
 import logging
 import os
-import re
 import subprocess
 import uuid
 from copy import deepcopy
 from functools import singledispatchmethod
-from pathlib import Path
-from typing import Iterable, Mapping, Union
+from typing import Iterable, Mapping
 
-from .utils import get_fragment_cache_path, get_model_repo_cache_path
+from .utils import get_fragment_cache_path, get_model_repo_cache_path, unwind1_key_attr
 
 
 def _get_fragment_path(fragment: Mapping):
@@ -19,12 +17,14 @@ def _get_fragment_path(fragment: Mapping):
     lfname = os.path.join(cchpth, fname)
     return cchpth, lfname
 
+
 def _get_repo_path(model: Mapping):
     cchpth, repo = get_model_repo_cache_path(model, create=False)
     repopath = os.path.join(cchpth, repo)
     return repopath
 
-def _compile_dataset_fragments(dataset : Mapping):
+
+def _compile_dataset_fragments(dataset: Mapping):
     """Creates a dictionary to access dataset fragments by ID"""
     dataset_context = {}
     if 'fragments' in dataset:
@@ -32,15 +32,16 @@ def _compile_dataset_fragments(dataset : Mapping):
             fragment_id = fragment['id']
             if fragment_id in dataset_context:
                 logging.warning("In dataset %s there are multiple fragments with id: %s", dataset['name'], fragment_id)
-            fragment_dir, fragment_path =  _get_fragment_path(fragment)
+            fragment_dir, fragment_path = _get_fragment_path(fragment)
             dataset_context[fragment['id']] = fragment_path
-            dataset_context[fragment['id']+'.dir'] = fragment_dir
+            dataset_context[fragment['id'] + '.dir'] = fragment_dir
 
     else:
-        fragment_dir, fragment_path =  _get_fragment_path(fragment)
+        fragment_dir, fragment_path = _get_fragment_path(fragment)
         dataset_context['main'] = fragment_path
         dataset_context['main.dir'] = fragment_dir
     return dataset_context
+
 
 class ContextExecutor:
     """
@@ -63,9 +64,10 @@ class ContextExecutor:
         return workdir
 
     @singledispatchmethod
-    def execute(self, commands : Iterable[str], context : Mapping, dryrun=False):
+    def execute(self, commands: Iterable[str], context: Mapping, dryrun=False):
         context = deepcopy(self.context)
         context.update(context)
+        context = unwind1_key_attr(context)
         # TODO: if we have different command sets, ensure workdir is unique
         workdir = self.prepare_workdir(self.config.get('workdir'))
         for command in commands:
@@ -78,8 +80,8 @@ class ContextExecutor:
                 logging.info("Executing [dry run]: '%s'", command)
             else:
                 logging.info("Executing: '%s'", command)
-                subprocess.run(command, shell=True, check=True, cwd=workdir) # Will raise exception if command failed
+                subprocess.run(command, shell=True, check=True, cwd=workdir)  # Will raise exception if command failed
 
     @execute.register
-    def execute_str(self, commands : str, context : Mapping, dryrun=False):
+    def execute_str(self, commands: str, context: Mapping, dryrun=False):
         return self.execute([commands], context, dryrun=False)
--- sabath/utils.py
+++ sabath/utils.py
@@ -6,14 +6,41 @@ import urllib
 import sabath
 
 
+class KeyOrAttr:
+    def __init__(self, basedict):
+        self.basedict = basedict
+
+    def __getitem__(self, key):
+        return self.basedict[key]
+
+    def __getattr__(self, attr):
+        try:
+            return self.basedict[attr]
+        except KeyError:
+            raise AttributeError(attr)
+
+
+def unwind1_key_attr(d):
+    print(d)
+    nd = dict()
+    for k in d:
+        if type(d[k]) is dict:
+            nd[k] = KeyOrAttr(d[k])
+        else:
+            nd[k] = d[k]
+    return nd
+
+
 def hashable(dct):
     "Create hashable string by  normalizing a JSON-serializable dictionary"
     # return the same dictionary with sorted keys
     return json.dumps({k: dct[k] for k in sorted(dct.keys())})
 
+
 def cache_path(name, kind):
     dgst = hashlib.sha256(name.encode()).hexdigest()
     return os.path.join(sabath.cache, dgst[:2], dgst[2:], kind)
+
 
 def get_fragment_cache_path(fragment, create=False):
     cchpth = cache_path(fragment["url"], "url")
@@ -22,6 +49,7 @@ def get_fragment_cache_path(fragment, create=False):
 
     base, fname = os.path.split(fragment["url"])
     return cchpth, fname
+
 
 def get_model_repo_cache_path(model, create=False):
     cchpth = cache_path(hashable(model["git"]), "git")
