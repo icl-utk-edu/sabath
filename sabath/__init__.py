4ddb6427a58f4bf6dfc00a390dcb0bb0d9c5bcca
Piotr Luszczek <luszczek@icl.utk.edu>
2023-10-09 01:30:40 +0000

    Create cache directory (if missing)
    Fixes issue #14

--- sabath/__init__.py
+++ sabath/__init__.py
@@ -11,6 +11,7 @@ if root is None:
     root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 cache = os.path.join(root, "var", "sabath", "cache")
+os.makedirs(cache, exist_ok=True)
 
 logging.basicConfig(filename=os.path.join(cache, "sabath.log"), encoding="utf-8", level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(message)s")
 
